383361b2fb3f4bd08570fce4b475610c97b8646b
Jiaming Guo <j23guo@gmail.com>
2022-08-26 10:13:33 -0400

    Initial Commit

---
+++ HackingWithiOS/HackingWithiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithiOSApp.swift
+//  HackingWithiOS
+//
+//  Created by Jiaming Guo on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
